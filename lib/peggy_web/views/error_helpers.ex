e8551c77d64ecff1b7d7b24dc74da309d004f3e9
Tan Kwang How <tankwanghow@gmail.com>
2021-07-12 15:09:37 +0800

    make bulma error input styling work with liveview

--- lib/peggy_web/views/error_helpers.ex
+++ lib/peggy_web/views/error_helpers.ex
@@ -8,10 +8,10 @@ defmodule PeggyWeb.ErrorHelpers do
   @doc """
   Generates tag for inlined form input errors.
   """
-  def error_tag(form, field, class \\ "invalid-feedback") do
+  def error_tag(form, field) do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
       content_tag(:span, translate_error(error),
-        class: class,
+        class: "is-danger help invalid-feedback",
         phx_feedback_for: input_name(form, field)
       )
     end)
