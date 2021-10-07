8b2ca2c4b62df74059613bfeea028c5c1c722c90
Tan Kwang How <tankwanghow@gmail.com>
2021-10-08 02:05:22 +0800

    hide invite user if not admin

--- test/peggy_web/controllers/navigation_controller_test.exs
+++ test/peggy_web/controllers/navigation_controller_test.exs
@@ -6,14 +6,26 @@ defmodule PeggyWeb.NavigationControllerTest do
   describe "Nagivation Live has active farm" do
     setup :user_set_active_farm
 
-    test "New Farm From Looks", %{conn: conn, farm: farm} do
+    test "Look and Feels", %{conn: conn, farm: farm} do
       conn = get(conn, Routes.navigation_path(conn, :index, farm.id))
       response = html_response(conn, 200)
       assert response =~ "id=\"navigation-title\""
       assert response =~ "Navigation Page"
       refute response =~ "id=\"home-button\""
     end
-  end
 
-  
+    test "Admin Look and Feels", %{conn: conn, farm: farm} do
+      conn = get(conn, Routes.navigation_path(conn, :index, farm.id))
+      response = html_response(conn, 200)
+      assert response =~ "Invite User"
+    end
+
+    test "Non-Admin Look and Feels", %{conn: conn, user: user, farm: farm} do
+      otheruser = Peggy.UserAccountsFixtures.user_fixture()
+      Peggy.Company.allow_user_access_farm(otheruser, farm, "guest", user)
+      conn = get(log_in_user(conn, otheruser), Routes.navigation_path(conn, :index, farm.id))
+      response = html_response(conn, 200)
+      refute response =~ "Invite User"
+    end
+  end
 end
