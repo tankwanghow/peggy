9933a9d09dec2d1401592016ede21e7dc9de36de
Tan Kwang How <tankwanghow@gmail.com>
2021-10-27 15:31:21 +0800

    bugsfix redirect to many time

--- lib/peggy_web/live/on_mount_func.ex
+++ lib/peggy_web/live/on_mount_func.ex
@@ -7,27 +7,22 @@ defmodule PeggyWeb.OnMountFunc do
   # that attach this module as an `on_mount` hook
   def on_mount(_, _params, session, socket) do
     set_locale(session)
+    socket = assign_current_user(socket, session)
 
-    {:cont,
-     socket
-     |> assign_current_user(session)
-     |> assign_current_farm_user(session)}
+    case session["current_farm_user"] do
+      nil -> {:cont, socket}
+
+      farm_user -> {:cont, assign(socket, :current_farm_user, farm_user)}
+    end
   end
 
-  defp set_locale(%{"locale" => locale}) do
-    Gettext.put_locale(PeggyWeb.Gettext, if(locale, do: locale, else: "en"))
+  defp set_locale(session) do
+    Gettext.put_locale(PeggyWeb.Gettext, if(session["locale"], do: session["locale"], else: "en"))
   end
 
   defp assign_current_user(socket, session) do
     assign_new(socket, :current_user, fn ->
       UserAccounts.get_user_by_session_token(session["user_token"])
     end)
-  end
-
-  defp assign_current_farm_user(socket, session) do
-    case session["current_farm_user"] do
-      nil -> push_redirect(socket, to: "/farms")
-      farm_user -> assign(socket, :current_farm_user, farm_user)
-    end
   end
 end
