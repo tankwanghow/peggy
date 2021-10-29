3e622425304baa77a3398bb7aa9936d8765b883d
Tan Kwang How <tankwanghow@gmail.com>
2021-10-29 23:45:37 +0800

    allow long logger msg

--- config/test.exs
+++ config/test.exs
@@ -26,7 +26,7 @@ config :peggy, PeggyWeb.Endpoint,
 config :peggy, Peggy.Mailer, adapter: Swoosh.Adapters.Test
 
 # Print only warnings and errors during test
-config :logger, level: :warn
+config :logger, level: :warn, truncate: :infinity
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
