bbd776162a4805eb898b3dadc1a849502e9612cf
Tan Kwang How <tankwanghow@gmail.com>
2021-11-03 17:12:32 +0800

    add constraint to DB

--- priv/repo/migrations/20211014074241_create_locations.exs
+++ priv/repo/migrations/20211014074241_create_locations.exs
@@ -3,11 +3,11 @@ defmodule Peggy.Repo.Migrations.CreateLocations do
 
   def change do
     create table(:locations) do
-      add :code, :string
+      add :code, :string, null: false
       add :note, :text
       add :capacity, :integer
-      add :status, :string
-      add :farm_id, references(:farms, on_delete: :delete_all)
+      add :status, :string, null: false
+      add :farm_id, references(:farms, on_delete: :delete_all), null: false
 
       timestamps()
     end
